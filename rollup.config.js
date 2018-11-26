af947d1a831716057b4ced881a0132dfa995047b
Simon Egeberg <egeberg.simon@gmail.com>
2018-11-26 14:25:52 +0100

    cocker

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,17 @@ import typescriptPlugin from "rollup-plugin-typescript2";
 import nodeResolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import { uglify } from "rollup-plugin-uglify";
+import builins from "rollup-plugin-node-builtins";
+import globals from "rollup-plugin-node-globals";
 
 const plugins = [
   // nodeResolve makes rollup look for dependencies in the node_modules directory
-  nodeResolve(),
+  globals(),
+  builins(),
+  nodeResolve({
+    jsnext: true,
+    main: true
+  }),
   commonjs({
     // All of our own sources will be ES6 modules, so only node_modules need to be resolved with cjs
     include: "node_modules/**",
@@ -21,15 +28,14 @@ const plugins = [
   })
 ];
 
-plugins.push(uglify());
-
 export default {
   input: "./src/main.ts",
-  sourceMap: true,
+  sourceMap: false,
   output: {
     file: "./build/bundle.js",
     format: "iife",
     name: "bundle"
   },
+
   plugins
 };
