ab5d8aa37bb2d9102f3aca3a1530e284a9a515be
Simon Egeberg <egeberg.simon@gmail.com>
2018-11-26 15:26:19 +0100

    all led showing

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,11 @@ import { Gpio } from "onoff";
 
 const gpio = require("onoff").Gpio;
 
-
-(new gpio(5, "out")as Gpio).writeSync(1);
-(new gpio(6, "out")as Gpio).writeSync(1);
-(new gpio(13, "out")as Gpio).writeSync(1);
-(new gpio(19, "out")as Gpio).writeSync(1);
-(new gpio(26, "out")as Gpio).writeSync(1);
-(new gpio(12, "out")as Gpio).writeSync(1);
-
-
+(new gpio(5, "out") as Gpio).writeSync(1);
+(new gpio(6, "out") as Gpio).writeSync(1);
+(new gpio(13, "out") as Gpio).writeSync(1);
+(new gpio(19, "out") as Gpio).writeSync(1);
+(new gpio(26, "out") as Gpio).writeSync(1);
+(new gpio(12, "out") as Gpio).writeSync(1);
+(new gpio(20, "out") as Gpio).writeSync(1);
+(new gpio(21, "out") as Gpio).writeSync(1);
