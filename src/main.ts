5ed648cefae7954f9bc7d044df39079c5c533164
Simon Egeberg <egeberg.simon@gmail.com>
2018-11-26 15:55:27 +0100

    interval

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { LedArray } from "./LedArray";
+import { on } from "cluster";
 
 const gpio = require("onoff").Gpio;
 
@@ -6,8 +7,30 @@ const Reds = new LedArray([26, 19, 13]);
 const Greens = new LedArray([5, 6, 12]);
 const Yellows = new LedArray([16, 20, 21]);
 
-[Reds, Greens, Yellows].forEach(ledArray => {
-  ledArray.GpioArray.forEach(led => {
-    led.Gpio = 1;
+let flag = false;
+
+const turnOn = () => {
+  [Reds, Greens, Yellows].forEach(ledArray => {
+    ledArray.GpioArray.forEach(led => {
+      led.Gpio = 1;
+    });
   });
-});
+};
+
+const turnOff = () => {
+  [Reds, Greens, Yellows].forEach(ledArray => {
+    ledArray.GpioArray.forEach(led => {
+      led.Gpio = 0;
+    });
+  });
+};
+
+setInterval(() => {
+  if (flag) {
+    turnOff();
+  }
+  if (!flag) {
+    turnOn();
+  }
+  flag = !flag;
+}, 1000);
