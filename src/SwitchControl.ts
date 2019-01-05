3db903c84591104735c8ed9e6679e4e09cce9f07
Simmetopia <egeberg.simon@gmail.com>
2019-01-05 13:06:16 +0100

    a thing i forgot

--- src/SwitchControl.ts
+++ src/SwitchControl.ts
@@ -1,12 +1,12 @@
 import {Gpio} from 'onoff';
 
-let gpio = require('onoff').GPIO;
+const Gpio = require('onoff').GPIO;
 export class SwitchControl {
   private _flipFlotState = 0;
   private _gpioPin: Gpio;
 
   constructor(gpioPin: number) {
-    this._gpioPin = new gpio(gpioPin, 'in', 'rising', {debounceTimeout: 10});
+    this._gpioPin = new Gpio(gpioPin, 'in', 'rising', {debounceTimeout: 10});
   }
 
   public startWatching = (cb: () => void) => {
