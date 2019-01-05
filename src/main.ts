384bd87132ea328a7af9082577e536f1aba9a1e0
Simmetopia <egeberg.simon@gmail.com>
2019-01-05 01:16:04 +0100

    small changes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import {LedArray} from './LedArray';
 const gpio = require('onoff').Gpio;
 
 const Mode1 = new gpio(17, 'in', 'both');
-
+console.log('starting to watch mode with :', JSON.stringify(Mode1));
 Mode1.watch((err: any, val: any) => {
   console.log('now watching for changes');
   if (err) console.log('there has been an error', err);
